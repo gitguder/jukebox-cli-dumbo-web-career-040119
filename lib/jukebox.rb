d2dcb0f6db5a39ba56e8bce0d7f9f9d2618a0051
Codey Pinero <codey.pinero@gmail.com>
2019-03-28 00:53:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
     if (1..9).to_a.include?(user_input.to_i)
       puts "Playing #{songs[user_input.to_i - 1]}"
     elsif songs.include?(song_to_play)
-      
+      puts "Playing #{user_input}"
       puts "Invalid input, please try again"
     else
 
